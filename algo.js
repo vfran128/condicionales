58e4e41a7eb3188b2a5a848ee18a479a3deddcb8
vfran128 <vallejosfran8@gmail.com>
2022-04-19 12:41:57 -0300

    primer commit

---
+++ algo.js
@@ -0,0 +1,15 @@
+//prompt() solo funciona en navegador.
+
+let a = prompt('Ingresa a')
+
+let b = prompt('Ingresa b')
+
+let c = prompt('Ingresa c')
+
+if (a == b && b == c){
+ console.log('Iguales')
+}
+
+else {
+ console.log('No iguales');
+}
